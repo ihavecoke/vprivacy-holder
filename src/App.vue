13b963d05ebba892b7fcff285dc29b851f3a7716
ihavecoke <ihavecoke@163.com>
2020-05-28 00:27:02 +0800

    More Demo case.

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,28 @@
         <h2>Phone Number</h2>
         <VPrivacyHolder raw-value="13800000000" privacy-strategy="mobile" />
       </div>
+      <div class="case">
+        <h2>Name</h2>
+        <VPrivacyHolder raw-value="ihave coke" privacy-strategy="name" />
+      </div>
+      <div class="case">
+        <h2>Email</h2>
+        <VPrivacyHolder
+          raw-value="ihavecoke@163.com"
+          privacy-strategy="email"
+        />
+      </div>
+      <div class="case">
+        <h2>ID Card</h2>
+        <VPrivacyHolder
+          raw-value="51012416372290938"
+          privacy-strategy="idCard"
+        />
+      </div>
+      <div class="case">
+        <h2>All Privacy</h2>
+        <VPrivacyHolder raw-value="allStringsWillPrivacy" />
+      </div>
     </div>
   </div>
 </template>
@@ -27,8 +49,14 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
   padding: 100px;
+  margin: 0 auto;
+  max-width: 500px;
 }
 .case {
   text-align: left;
+}
+.case + .case {
+  border-top: 1px solid #ccc;
+  margin-top: 16px;
 }
 </style>
